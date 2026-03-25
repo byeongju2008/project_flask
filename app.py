6feb17053d0398d0254d74242782bce55eff7e9e
Byeongju <byeongju2008@gmail.com>
2026-03-25 11:04:10 +0900

    나만의 프로필 api(json 반환)

--- app.py
+++ app.py
@@ -3,22 +3,14 @@ import random
 
 app = Flask(__name__)
 
-activities = [
-    "산책하기",
-    "영화 보기",
-    "운동하기",
-    "책 읽기",
-    "게임하기"
-]
-
 @app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/recommend")
-def recommend():
-    activity = random.choice(activities)
-    return render_template("result.html", activity=activity)
+def my_profile():
+    return{
+        "name":"송병주",
+        "role":"초보 서버 개발자",
+        "status":"로컬 환경 통제 완료!",
+        "skills":["Ubuntu","VS Code","Python","Flask"],
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
