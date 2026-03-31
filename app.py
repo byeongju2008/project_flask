682f07665d1cdecb168399befe723ccb88f544cb
Byeongju <byeongju2008@gmail.com>
2026-04-01 08:51:27 +0900

    html 파일 보내기

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def my_profile():
-    return{
-        "name":"송병주",
-        "role":"초보 서버 개발자",
-        "status":"로컬 환경 통제 완료!",
-        "skills":["Ubuntu","VS Code","Python","Flask"],
-    }
+@app.route('/')
+def home():
+    # 이제는 JSON이 아니라 HTML 파일을 보냅니다
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
