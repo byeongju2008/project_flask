477995054a9761471101dd6593d95124f0312fca
Byeongju <byeongju2008@gmail.com>
2026-04-01 09:02:29 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    #여기에 내 데이터를 만든다!
+    my_profile = {
+        "name": "송병주",
+        "age": "만17,19세"
+    }
     # 이제는 JSON이 아니라 HTML 파일을 보냅니다
     return render_template('index.html')
 
