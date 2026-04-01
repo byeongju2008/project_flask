89ee73d002b76dc7d9135b6b25cdca1f2609ccc9
Byeongju <byeongju2008@gmail.com>
2026-04-01 09:19:38 +0900

    수정

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def home():
     #여기에 내 데이터를 만든다!
     my_profile = {
         "name": "송병주",
-        "age": "만17,19세"
+        "age": "18",
+        "school": "종로산업정보학교",
+        "hobby": "게임, 그림, 음악"
+
     }
+
     # 이제는 JSON이 아니라 HTML 파일을 보냅니다
     return render_template('index.html', data=my_profile)
 
